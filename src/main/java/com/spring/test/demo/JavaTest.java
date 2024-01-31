d033e70d5e74a99dfba7adf7e3cfd3659eba0b71
阿提说说 <0431cjj@163.com>
2024-01-31 19:50:41 +0800

    test

---
+++ src/main/java/com/spring/test/demo/JavaTest.java
@@ -0,0 +1,13 @@
+//package com.spring.test.demo;
+//
+//import org.apache.commons.logging.Log;
+//import org.apache.commons.logging.LogFactory;
+//
+//public class JavaTest {
+//
+//    private static Log log = LogFactory.getLog(JavaTest.class);
+//
+//    public static void main(String[] args) {
+//        log.info("test");
+//    }
+//}
--- src/main/java/com/spring/test/demo/SpringTest.java
+++ src/main/java/com/spring/test/demo/SpringTest.java
@@ -1,14 +1,22 @@
 package com.spring.test.demo;
 
 import com.spring.test.demo.bean.Bean;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.logging.Logger;
+
 public class SpringTest {
+
+    public static final Log log = LogFactory.getLog(SpringTest.class);
     public static void main(String[] args) {
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
         applicationContext.register(Bean.class);
         applicationContext.refresh();
         Bean bean = applicationContext.getBean(Bean.class);
         bean.print();
+
+        log.info("ttest");
     }
 }
